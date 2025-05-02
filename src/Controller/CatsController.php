962b3295901fc3c179368aa95c41627cec7d1670
Sabrina <sabrinah_38@hotmail.fr>
2025-05-02 20:07:55 +0200

    initialisation boostrap

--- src/Controller/CatsController.php
+++ src/Controller/CatsController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Cats;
+use App\Entity\Caractere;
 use App\Enum\Gender;
+use App\Repository\CaractereRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,11 +22,9 @@ class CatsController extends AbstractController
         $this->entityManager = $entityManager;
     }
 
-    // Affiche la liste des chats de l'utilisateur
     #[Route('/cats', name: 'app_list_cats', methods: ['GET'])]
     public function listCats(UserInterface $user): Response
     {
-        // Récupère les chats associés à l'utilisateur
         $cats = $this->entityManager->getRepository(Cats::class)->findBy(['user' => $user]);
 
         return $this->render('cats/list.html.twig', [
@@ -32,44 +32,61 @@ class CatsController extends AbstractController
         ]);
     }
 
-    // Afficher le formulaire pour ajouter un chat
     #[Route('/cats/add/form', name: 'app_add_cat_form', methods: ['GET'])]
-    public function addCatForm(): Response
+    public function addCatForm(CaractereRepository $caractereRepo): Response
     {
-        return $this->render('cats/add_cat.html.twig');
+        $caracteres = $caractereRepo->findAll();
+        return $this->render('cats/add_cat.html.twig', [
+            'caracteres' => $caracteres
+        ]);
     }
 
-    // Ajouter un chat
     #[Route('/cats/add', name: 'app_add_cat', methods: ['POST'])]
-    public function addCat(Request $request, UserInterface $user): Response
+    public function addCat(Request $request, UserInterface $user, CaractereRepository $caractereRepo): Response
     {
         $name = $request->request->get('name');
         $age = $request->request->get('age');
         $breed = $request->request->get('breed');
         $genderValue = $request->request->get('gender');
-        $gender = Gender::from($genderValue);  // Utilisation de l'enum Gender
+        $description = $request->request->get('description');
+        $caractereIds = $request->request->all('caracteres');
+
+        $gender = Gender::from($genderValue);
 
         if (!$user) {
             return $this->json(['message' => 'Vous devez être connecté pour ajouter un chat.'], 403);
         }
 
-        // Création du chat et association avec l'utilisateur
         $cat = new Cats();
         $cat->setName($name);
         $cat->setAge($age);
         $cat->setBreed($breed);
-        $cat->setGender($gender);  // Utilisation de l'enum Gender
+        $cat->setGender($gender);
+        $cat->setDescription($description);
+        $cat->setImageId(random_int(1, 10));
         $cat->setUser($user);
 
-        // Persister le chat dans la base de données
+        foreach ($caractereIds as $id) {
+            $caractere = $caractereRepo->find($id);
+            if ($caractere) {
+                $cat->addCaractere($caractere);
+            }
+        }
+
         $this->entityManager->persist($cat);
         $this->entityManager->flush();
 
-        // Réponse de succès
-        return $this->json(['message' => 'Chat ajouté avec succès.'], 200);
+        return $this->redirectToRoute('app_cat_success', ['id' => $cat->getId()]);
     }
 
-    // Mise à jour du chat (formulaire et traitement)
+    #[Route('/cats/success/{id}', name: 'app_cat_success', methods: ['GET'])]
+    public function success(Cats $cat): Response
+    {
+        return $this->render('cats/success.html.twig', [
+            'cat' => $cat
+        ]);
+    }
+
     #[Route('/cats/update/{id}', name: 'app_update_cat', methods: ['POST'])]
     public function updateCat(Request $request, Cats $cat, UserInterface $user): Response
     {
@@ -81,9 +98,8 @@ class CatsController extends AbstractController
         $age = $request->request->get('age');
         $breed = $request->request->get('breed');
         $genderValue = $request->request->get('gender');
-        $gender = Gender::from($genderValue);  // Utilisation de l'enum Gender
+        $gender = Gender::from($genderValue);
 
-        // Mise à jour des informations du chat
         if (!empty($name)) {
             $cat->setName($name);
         }
@@ -94,10 +110,9 @@ class CatsController extends AbstractController
             $cat->setBreed($breed);
         }
         if (!empty($gender)) {
-            $cat->setGender($gender);  // Utilisation de l'enum Gender
+            $cat->setGender($gender);
         }
 
-        // Sauvegarde des changements
         $this->entityManager->flush();
 
         return $this->json(['message' => 'Chat mis à jour avec succès.'], 200);
